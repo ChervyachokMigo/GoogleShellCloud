503c4e7c9259b55214f581bbc41d667bc01a9c14
Chervethread <74270643+ChervyachokMigo@users.noreply.github.com>
2021-02-28 17:31:52 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd cpuminer-opt
-sudo ./cpuminer -u ВВЕДИТЕ_НОМЕР_КОШЕЛЬКА.GOOGLE_POWER -a yespowerR16 -o stratum+tcp://2chpool.cc:63368 -q -N 2048 -T 1000 --cpu-priority 0 --randomize
+./cpuminer -u ВВЕДИТЕ_НОМЕР_КОШЕЛЬКА.GOOGLE_POWER -a yespowerR16 -o stratum+tcp://2chpool.cc:63368 -q -N 2048 -T 1000 --cpu-priority 0 --randomize
